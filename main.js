df10139ad8095dce8c280a2f3df3c96e7b0e5273
Moses-TM <mosesmagar97@gmail.com>
2025-07-24 03:16:50 +0000

    Update button functionality for calculator interface

--- main.js
+++ main.js
@@ -1,46 +1,35 @@
-function add(num1, num2){
-    return num1 + num2;
+const inputDisplay = document.getElementById('input');
+const outputDisplay = document.getElementById('output');
+const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('delete');
+const equalBtn = document.getElementById('equal');
+const numberBtn = document.querySelectorAll('.numbers');
+const operatorBtn = document.querySelectorAll('.operator');
+
+let initialNumber = null;
+let presentNumber = '0';
+let currentOperator = null;
+let resultNumber = null;
+
+function getNumbers(){
+    
 }
 
-function subtract(num1, num2){
-    return num1 - num2;
+function display(item){
+    inputDisplay.textContent += item
 }
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function divide(num1, num2){
-    return num1 / num2;
-}
-
-const inputDisplay = document.getElementById('input')
-const outputDisplay = document.getElementById('output')
-const display = document.getElementById('display')
-const clearBtn = document.getElementById('clear')
-const deleteBtn = document.getElementById('delete')
-const equalBtn = document.getElementById('equal')
-const buttonDisplay = document.querySelectorAll('.display')
 
 function operate(){
-    //CLEAR BUTTON
-    clearBtn.addEventListener('click', () => {
-        inputDisplay.textContent = '';
-        outputDisplay.textContent = '';
-    })
-
-    //DELETE BUTTON
-    deleteBtn.addEventListener('click', () => {
-        let currentText = inputDisplay.textContent
-        inputDisplay.textContent = currentText.slice(0, -1)
-    })
-
-    //DISPLAY
-    buttonDisplay.forEach(btn => {
-        btn.addEventListener('click', () => {
-            inputDisplay.textContent += `${btn.textContent}`
-        })
-    })
+    
 }
 
-operate();
+document.addEventListener('DOMContentLoaded', () => {
+    //NUMBER BUTTONS
+    numberBtn.forEach(button => {
+        button.addEventListener('click', () => {
+            display(button.textContent)
+        })
+    })
+
+    
+})
