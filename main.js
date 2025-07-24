b4e1d9fefa7c11d05000d982aea7ca41b263990b
Moses-TM <mosesmagar97@gmail.com>
2025-07-24 03:44:38 +0000

    Refactor calculator functions to improve number handling and error display

--- main.js
+++ main.js
@@ -11,25 +11,88 @@ let presentNumber = '0';
 let currentOperator = null;
 let resultNumber = null;
 
-function getNumbers(){
-    
+function getNumbers(number){
+    if(presentNumber == '0'){
+        presentNumber = number;
+    }
+    else{
+        presentNumber += number;
+    }
 }
 
 function display(item){
-    inputDisplay.textContent += item
+    if(item == 'error'){
+        outputDisplay.textContent = '';
+        inputDisplay.textContent = 'MATH ERROR';
+        return;
+    }
+    else if(inputDisplay.textContent == 'MATH ERROR'){
+        inputDisplay.textContent = '';
+    }
+    outputDisplay.textContent = '';
+    inputDisplay.textContent += item;
 }
 
-function operate(){
-    
+function operate(operator){
+    if(initialNumber == null && resultNumber == null){
+        initialNumber = parseFloat(presentNumber);
+    }
+    else if(currentOperator){
+        let result = calculate(currentOperator);
+        initialNumber = result.toString();
+        inputDisplay.textContent = '';
+        display(result);
+    }
+
+    currentOperator = operator;
+    presentNumber = '0';
+}
+
+function calculate(operator){
+    let firstNumber = parseFloat(initialNumber);
+    let secondNumber = parseFloat(presentNumber);
+
+    switch(operator){
+        case '+':
+            return firstNumber + secondNumber;
+
+        case '-':
+            return firstNumber - secondNumber;
+
+        case 'x':
+            return firstNumber * secondNumber;
+
+        case '÷':
+            if(secondNumber == 0){
+                display("error");
+                initialNumber = null;
+                presentNumber = '0';
+                currentOperator = null;
+                resultNumber = null;
+                return;
+            }
+            else{
+                return firstNumber / secondNumber;
+            }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     //NUMBER BUTTONS
     numberBtn.forEach(button => {
+        let item = button.textContent;
         button.addEventListener('click', () => {
-            display(button.textContent)
+            display(item);
+            getNumbers(item);
         })
     })
 
-    
+    //OPERATOR BUTTONS
+    operatorBtn.forEach(button => {
+        let item = button.textContent;
+        button.addEventListener('click', () => {
+            operate(item);
+            display(item);
+        })
+    })
 })
